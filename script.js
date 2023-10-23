c2d686c8e2edebc42cb1de53cc23d47212b75c1e
safouanemeniaoui <safouane.meniaoui@gmail.com>
2023-10-23 15:17:17 +0100

    add score table

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let game = document.querySelector(".game");
 let boxs = document.querySelectorAll(".box");
 let score = document.querySelector(".score span");
 let player = document.querySelector(".name span");
@@ -7,36 +8,30 @@ let successAudio = document.querySelector(".success-aud");
 let failureAudio = document.querySelector(".failure-aud");
 let finalScore = document.querySelector(".final-score");
 let finalScoreSpan = document.querySelector(".final-score span");
+let scoreIcon = document.querySelector(".score-icon");
+let scoresList = document.querySelector(".scores-list");
+let closeScoreList = document.querySelector(".btn .close");
+let clearScoreList = document.querySelector(".btn .clear");
+let scoreTable = document.querySelector(".scores-list table tbody");
+
 let newArray = Array.from(boxs);
+let orderArray = [...newArray.keys()];
+let errorTry = 0,
+  finish = 17;
 
-let val_1 = 0,
-  val_2 = 0,
-  counter = 0,
-  errorTry = 0,
-  finish = 0,
-  f = 0,
-  l = 0;
+// Shuffle
 
-// start game
+shuffle(orderArray);
+
+// Start game
 
 start.onclick = function () {
   overlay.style.display = "none";
   start.style.display = "none";
   let name = window.prompt("Enter your name");
-  player.innerHTML = name != "" ? name : "player";
+  player.innerHTML = name != "" && name != null ? name : "player";
+  startGame();
 };
-
-// Shuffle
-
-let orderArray = [...newArray.keys()];
-function shuffle(tab) {
-  tab.forEach((el) => {
-    let random = Math.trunc(Math.random() * tab.length);
-    [tab[tab.indexOf(el)], tab[random]] = [tab[random], tab[tab.indexOf(el)]];
-  });
-  return tab;
-}
-shuffle(orderArray);
 
 // Main game function
 
@@ -44,36 +39,16 @@ newArray.forEach((box, index) => {
   box.style.order = orderArray[index];
   box.onclick = function () {
     box.classList.toggle("rotate");
-    counter++;
-    if (val_1 == 0) {
-      val_1 = box.dataset.set;
-      f = newArray.indexOf(box);
-      box.classList.add("correct");
-    }
-    if (counter == 2) {
-      l = newArray.indexOf(box);
-      val_2 = box.dataset.set;
-      if (val_2 == val_1 && f != l) {
-        finish++;
-        successAudio.play();
-        check(newArray, val_1);
-        check(newArray, val_1);
-      } else {
-        score.innerHTML = ++errorTry;
-        failureAudio.play();
-        newArray[f].classList.remove("correct");
-        setTimeout(() => {
-          newArray.forEach((bx) => {
-            bx.classList.remove("rotate");
-          });
-        }, 500);
-      }
-      counter = val_1 = val_2 = 0;
+    let roratedBoxes = newArray.filter((el) => el.classList.contains("rotate"));
+    if (roratedBoxes.length === 2) {
+      stopClicking();
+      matched(roratedBoxes[0], roratedBoxes[1]);
     }
     if (finish == 18) {
       finalScoreSpan.innerHTML = errorTry;
       finalScore.style.display = "block";
       overlay.style.display = "block";
+      saveLocal(player.innerHTML, errorTry);
       setTimeout(() => {
         location.reload();
       }, 4000);
@@ -81,11 +56,111 @@ newArray.forEach((box, index) => {
   };
 });
 
-function check(array = [], val) {
-  array.forEach((bx) => {
-    if (bx.dataset.set == val) {
-      bx.classList.add("correct");
-      array.splice(array.indexOf(bx), 1);
-    }
+// Scores
+
+if (window.localStorage.getItem("score")) {
+  fillScoreTable();
+}
+
+scoreIcon.onclick = function () {
+  scoresList.classList.add("show-score");
+  overlay.style.display = "block";
+};
+closeScoreList.onclick = function () {
+  scoresList.classList.remove("show-score");
+  overlay.style.display = "none";
+};
+clearScoreList.onclick = function () {
+  window.localStorage.clear();
+  scoresList.classList.remove("show-score");
+  overlay.style.display = "none";
+  scoreTable.innerHTML = "";
+};
+
+// Functions
+
+function matched(box_1, box_2) {
+  if (box_1.dataset.set === box_2.dataset.set) {
+    box_1.classList.remove("rotate");
+    box_2.classList.remove("rotate");
+    box_1.classList.add("matched");
+    box_2.classList.add("matched");
+    finish++;
+    successAudio.play();
+  } else {
+    score.innerHTML = ++errorTry;
+    failureAudio.play();
+    setTimeout(() => {
+      box_1.classList.remove("rotate");
+      box_2.classList.remove("rotate");
+    }, 1000);
+  }
+}
+
+function stopClicking() {
+  game.classList.add("block-game");
+  setTimeout(function () {
+    game.classList.remove("block-game");
+  }, 1000);
+}
+
+function startGame() {
+  newArray.forEach((box) => {
+    box.classList.add("rotate");
+  });
+  setTimeout(function () {
+    newArray.forEach((box) => {
+      box.classList.remove("rotate");
+    });
+  }, 2000);
+}
+
+function shuffle(tab) {
+  tab.forEach((el) => {
+    let random = Math.trunc(Math.random() * tab.length);
+    [tab[tab.indexOf(el)], tab[random]] = [tab[random], tab[tab.indexOf(el)]];
+  });
+  return tab;
+}
+
+function getDate() {
+  let date = new Date();
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  return (result = `${day}/${month}/${year}`);
+}
+
+function saveLocal(name, errors) {
+  let tab = [];
+  let newObj = {};
+  newObj.name = name;
+  newObj.errors = errors;
+  newObj.date = getDate();
+
+  if (window.localStorage.getItem("score")) {
+    tab = JSON.parse(window.localStorage.getItem("score"));
+    tab.push(newObj);
+    window.localStorage.setItem("score", JSON.stringify(tab));
+  } else {
+    tab.push(newObj);
+    window.localStorage.setItem("score", JSON.stringify(tab));
+  }
+}
+
+function fillScoreTable() {
+  let newTab = JSON.parse(window.localStorage.getItem("score"));
+  newTab.forEach((el) => {
+    let tr = document.createElement("tr");
+    let player = document.createElement("td");
+    let score = document.createElement("td");
+    let date = document.createElement("td");
+    player.innerHTML = el.name;
+    score.innerHTML = el.errors;
+    date.innerHTML = el.date;
+    tr.appendChild(player);
+    tr.appendChild(score);
+    tr.appendChild(date);
+    scoreTable.appendChild(tr);
   });
 }
